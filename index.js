a286836831ef981007b11c85d1e6ee970f43fffc
Ranga Prakash S <RangaPrakashs@users.noreply.github.com>
2019-03-11 17:25:31 +0530

    added string decoder

--- index.js
+++ index.js
@@ -3,43 +3,48 @@
  * Primary file for the API
  * 
  */
-
-const http = require('http');
-const url = require('url');
+const http = require( 'http' );
+const url = require( 'url' );
+const StringDecoder = require( 'string_decoder' ).StringDecoder;
 //Configure the server to respond to all calls with a string on Port 3000
-let server = http.createServer(function(req, res) {
-   //ignore favicon.
-    if (req.url != '/favicon.ico') {
+let server = http.createServer( function( req, res ) {
+	//ignore favicon.
+	if ( req.url != '/favicon.ico' ) {
+		//get the url
+		let parsedURL = url.parse( req.url, true );
+		//get the url path
+		let path = parsedURL.pathname;
+		let trimmedPath = path.replace(/^\/+|\/+$/g, '' );
+		//get the request method
+		let method = req.method.toLowerCase();
+		//get the query object.
+		let querystr = parsedURL.query;
+		//get the payloads
+		let decoder = new StringDecoder( 'utf-8' );
+        let buffer = '';
+        //Write data of the request as payload as utf-8
 
-        //get the url
-        let parsedURL = url.parse(req.url, true);
 
-        //get the url path
-        let path = parsedURL.pathname;
-        let trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-        //get the request method
-        let method = req.method.toLowerCase();
-
-        //get the query object.
-        let querystr = parsedURL.query;
-
-
-        
-        //send response header
-        res.writeHead(200, {
-            'Content-type': 'text/plain'
+		req.on( 'data', function( data ){ 
+			buffer += decoder.write( data );
         });
-
-        //send response text
-        res.end('Hello World!\n');
-
-
-        //log the request and method.
-        console.log('Request recieved on path : ' + trimmedPath +
-            '\nWith method : ' + method + '\nQuery Object : ', querystr);
-    }
-});
-server.listen(3000, function() {
-    console.log("Server is running noe")
-});
+        
+        //end method on 
+		req.on( 'end', function() {
+			buffer += decoder.end();
+			//send response header
+			res.writeHead( 200, {
+				'Content-type': 'text/plain'
+            } );
+            
+			///send response on end of the request.
+			res.end( 'Hello World!\n' );
+			console.log( "Request was recieved : " + buffer );
+        } );
+        
+		//log the request and method.
+	}
+} );
+server.listen( 3000, function() {
+	console.log( "Server is running now" )
+} );
